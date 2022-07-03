373467dad657a69ba99c3083d10a6da4280bdcc3
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-03 13:19:50 +0600

    creating user documents with firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC9C0VyyGIK7G20LaCj9b_ggTF5Ykd4HkI',
@@ -29,7 +29,23 @@ export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid);
   const userSnapShot = await getDoc(userDocRef);
+
   console.log(userDocRef);
   console.log(userSnapShot);
   console.log(userSnapShot.exists());
+
+  // if user data does not exists - create / set document with the data from userAuth in my collection
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log('Error creating the user', error.message);
+    }
+  }
+
+  // if user data exists - return userDocRef
+  return userDocRef;
 };
